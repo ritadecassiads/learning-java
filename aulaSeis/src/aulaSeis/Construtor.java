75cb6abeb73e1a842f4b0ec0e13aaea16ca2a0b4
Rita de Cássia Damasceno <104233304+ritadecassiads@users.noreply.github.com>
2023-04-12 14:12:38 -0300

    Delete aulaSeis directory

--- aulaSeis/src/aulaSeis/Construtor.java
+++
@@ -1,8 +0,0 @@
-package aulaSeis;
-
-// ao instanciar um objeto os atributos começam com valores padroes(null para string e 0 para int/float/double)
-public class Construtor {
-	public static void main(String[] args) {
-		
-	}
-}
